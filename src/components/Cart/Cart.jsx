f1c2aaf4b39f52a47fc180a92295f7257d669277
Retina04 <retinaislam2019nida@gmail.com>
2023-03-29 11:25:25 +0600

    card compononent added

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -5,17 +5,23 @@ const Cart = ({cart}) => {
   // const cart = props.cart;
   // const {cart} = props
   let total =0;
+  let shipping = 0;
+  
   for(const product of cart ){
     total = total + product.price;
+    shipping = shipping + product.shipping;
+
   }
+  const tax = total * 7 /100;
+  const grandTotal = total + tax + shipping;
   return (
     <div className="cart">
       <h3>Order Summary</h3>
       <p>Selected Items : {cart.length}</p>
       <p>Total price : {total}</p>
-      <p>Total Shipping :</p>
-      <p>Tax :</p>
-      <h6>Grand Total :</h6>
+      <p>Total Shipping : {shipping}</p>
+      <p>Tax : ${tax.toFixed(2)}</p>
+      <h6>Grand Total : {grandTotal.toFixed(2)}</h6>
     </div>
   );
 };
